9add8139e1d7e405c5544f92de2bf06235c34a9d
Tigran-Minasyan <31685908+Tigran-Minasyan@users.noreply.github.com>
2017-12-10 18:38:40 +0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -32,6 +32,10 @@ const ninjaImg = new Image();
 ninjaImg.src = "ninja.png";
 const background1 = new Image();
 background1.src = "cafeteria.jpg";
+const APointImg = new Image();
+APointImg.src = "A-Point.png";
+const FPointImg = new Image();
+FPointImg.src = "F-Point.png";
 
 const leftKey = 37;
 const upKey = 38;
@@ -52,7 +56,7 @@ const gameData = {
         multimouth: {
             pic: multimouthImg,
             x: 150,
-            y: 0,
+            y: 000,
             xDelta: 0,
             yDelta: 0,
             w: 250,
@@ -61,7 +65,7 @@ const gameData = {
         devil: {
             pic: devilImg,
             x: 450,
-            y: 0,
+            y: 000,
             xDelta: 0,
             yDelta: 0,
             w: 250,
@@ -70,7 +74,7 @@ const gameData = {
         octopus: {
             pic: octopusImg,
             x: 700,
-            y: 0,
+            y: 000,
             xDelta: 0,
             yDelta: 0,
             w: 250,
@@ -95,10 +99,17 @@ const gameData = {
         w: 0,
         h: 0
     },
+    background: {
+        pic: background1,
+        x: 0,
+        w: 1500
+    },
     score: {
         score: 0,
         APoint: 0,
-        FPoint: 0
+        FPoint: 0,
+        lvlCountA: [25,20,15,15],
+        lvlCountF: [5,7,9,11]
     }
 } 
 
@@ -109,13 +120,19 @@ const devil = gameData.monsters.devil;
 const exam = gameData.monsters.exam;
 const ninja = gameData.ninja;
 const monsters = [multimouth, octopus, devil/*, exam*/];
+const APoint = gameData.score.APoint;
+const FPoint = gameData.score.FPoint;
+const score = gameData.score;
+const background = gameData.background;
+const arrayA = [];
+const arrayF = [];
 var isJumping = false;
 var isFalling = false;
 var imgNum = 0;
 var isMoving = false;
+var level = 1;
 
 const forEach = function(arr, func) {
-
     const helper = function(index) {
         if(index === arr.length){
             return;
@@ -123,13 +140,62 @@ const forEach = function(arr, func) {
         func(arr[index]);   
         helper(index+1);    
     }
-
     helper(0);
 }
 
+const createPoints = function(level){
+
+	const pointsLoop = function(count, arr, pic, point){
+
+		if(count<=0){
+			return;
+		}
+
+		point[arr] = {
+            img: pic,
+            x: rand(19*background.w),
+            y: rand(300)+300,
+        }
+
+		return pointsLoop(count-1,arr+1, pic, point);
+	}
+
+    pointsLoop(score.lvlCountA[level-1], 0, APointImg, arrayA);
+    pointsLoop(score.lvlCountF[level-1], 0, FPointImg, arrayF);
+}
+createPoints(1);
+
+const drawPoints = function(){
+    const makePoints = function(arr,point,img){
+
+        if(arr === point.length){
+            return;
+        }
+
+        context.drawImage(img,point[arr].x,point[arr].y,30,30);
+        return makePoints(arr+1);
+    }
+
+    makePoints(0,arrayA,APointImg);
+    makePoints(0,arrayF,FPointImg);
+}
+
+
 const draw = function(){
 
-    context.drawImage(background1,0,0,1500,600)
+    for(i=0; i<=19; i++){
+        context.drawImage(background.pic,background.x + i*background.w,0,background.w,canvas.height);
+    }
+    
+    context.font = '20px Arial'
+    context.drawImage(APointImg,0,0,30,30);
+    context.drawImage(FPointImg,0,40,30,30);
+    context.fillStyle = 'yellow';
+    context.fillText('X',40,21);
+    context.fillText(APoint,65,21)
+    context.fillStyle = 'red';    
+    context.fillText('X',40,63);    
+    context.fillText(FPoint,65,63)
 
     forEach(monsters, function(monsters){
         context.drawImage(monsters.pic, monsters.x, monsters.y, monsters.w, monsters.h);
@@ -140,16 +206,8 @@ const draw = function(){
     move();
 }
 
-const moveRight = function(){
-
-    if(imgNum <= 6){
-        imgNum ++;
-    } else {
-        imgNum = 0;
-    }
-}
-
 const move = function(){
+
     if(isMoving === true){
         if(imgNum <= 6){
             imgNum ++;
@@ -158,6 +216,10 @@ const move = function(){
         }
     } else {
         imgNum = 0;
+    }
+
+    if(isMoving === true){
+        background.x -= 5;
     }
     
 }
@@ -170,6 +232,7 @@ const update = function(){
 const jump = function(){
 
     if(isJumping === true){
+        imgNum = 4;
         if(isFalling === false){
             hero.yDelta = -5;
             if(hero.y === 200){
@@ -192,6 +255,7 @@ const loop = function(){
     update();
     draw();
     jump();
+    // drawPoints();
     requestAnimationFrame(loop);
 }
 
